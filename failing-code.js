06cff7ff2b0f6ee9dca97f7a5cec91dc43b39734
Arnout Kazemier <info@3rd-Eden.com>
2017-11-30 21:48:25 +0100

    [major] Internal refactor to allow passing of custom file passer

--- failing-code.js
+++ failing-code.js
@@ -1,32 +1,43 @@
-var isNode = !!(typeof process != 'undefined' && process.versions && process.versions.node);
 var failingLine = require('./failing-line');
-
-var fs;
 var nodeRequire;
+var fs;
 
-if (isNode) {
+//
+// wut, yeah, this is for browserify to prevent it from bundling
+// a file system polyfill.
+//
+if (require('is-Node')) {
   nodeRequire = require;
   fs = nodeRequire('fs');
   nodeRequire = null;
 }
 
-module.exports = function failingCode (error, doc, shift) {
-  var ln = failingLine(error, shift);
+/**
+ * Failing code.
+ *
+ * @param {Error} error Error.
+ * @param {Object} options Configuration.
+ * @returns {Undefined|Array} Parsed failed code.
+ * @private
+ */
+function failingCode(error, options) {
+  var ln = failingLine(error, options.shift);
 
   if (!ln) return;
 
+  var doc = options.read && options.read(ln);
   if (!doc && fs) {
     try {
       doc = fs.readFileSync(ln.filename).toString();
-    } catch (readError) {
+    } catch (e) {
       return undefined;
     }
   }
 
   if (!doc) return undefined;
 
+  var lines = typeof doc === 'string' ? doc.split('\n') : doc;
   var result = [];
-  var lines = doc.split('\n');
 
   var i = ln.line - 3;
   while (++i < ln.line + 1) {
@@ -35,6 +46,7 @@ module.exports = function failingCode (error, doc, shift) {
         line: ln.line - (ln.line - i -1),
         code: lines[i]
       });
+
       continue;
     }
 
@@ -49,4 +61,9 @@ module.exports = function failingCode (error, doc, shift) {
   }
 
   return result;
-};
+}
+
+//
+// Expose the module.
+//
+module.exports = failingCode;
